0947c80696b481dde91d4516df9e16b08c007f1b
Guille <57747085+Byguillemc21@users.noreply.github.com>
2021-10-19 23:48:30 +0200

    Proj
    Proj

--- MP12/PROJECTE-2/src/index.js
+++
@@ -1,5 +0,0 @@
-//inicio
-const app = require('./app');
-
-app.listen(app.get('port'));
-console.log('Server on port ', app.get('port'));
--- MP12/PROJECTE-2/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-router.get('/', (req, res) => {
-    console.log('Indexx works!');
-    res.send('received');
-})
